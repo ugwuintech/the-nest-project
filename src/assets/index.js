39a01468919750d693c34abc29ecc54b8e7aafb0
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-03-28 18:30:47 -0700

    completed testimonial

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,6 @@ import slide1 from "./slide 1.png";
 import slide2 from "./2ndslide.png";
 import slide3 from "./3rdslide.png";
 import benefits from "./benefits.png";
+import rebecca from "./Abena Rebecca.png";
 
-export { menu, close, logo, slide1, slide2, slide3, benefits };
+export { menu, close, logo, slide1, slide2, slide3, benefits, rebecca };
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { styles } from "../styles";
+import { rebecca } from "../assets";
+import { BsStarFill } from "react-icons/bs";
+import { BsStar } from "react-icons/bs";
 
 const Testimonial = () => {
   return (
@@ -7,6 +10,31 @@ const Testimonial = () => {
       <h1 className={`${styles.sectionHeadText} text-center`}>
         Our Client's Feedback
       </h1>
+      <div className="flex lg:flex-row flex-col gap-10">
+        <div className="image">
+          <img src={rebecca} alt="benefits image" />
+        </div>
+        <div className="flex flex-col lg:w-[500px] w-[250px]">
+          <div>
+            <h1 className=" tracking-wider lg:text-[40px] sm:text-[40px] xs:text-[25px] text-[30px] lg:leading-[68px]">
+              Abena Rebecca
+            </h1>
+            <p>Tourist </p>
+            <div className="ratings flex flex-row mt-3">
+              <BsStarFill size="2em" className="icon" color="gold" />
+              <BsStarFill size="2em" className="icon" color="gold" />
+              <BsStarFill size="2em" className="icon" color="gold" />
+              <BsStarFill size="2em" className="icon" color="gold" />
+              <BsStar size="2em" className="icon" color="gold" />
+            </div>
+            <h2 className="tracking-wider mt-3 lg:text-[20px] sm:text-[18px] xs:text-[10px] text-[18px]">
+              Marvel Travel has given me the most amazing travel experience I've
+              had in a while. I will definitely recommend Marvel Travel to
+              anyone
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

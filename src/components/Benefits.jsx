bcfbb6e8f9a2f48cbc52c607ef08e662b93a064a
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-04-01 04:58:39 -0700

    updated benefits motion

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -6,7 +6,7 @@ import { RiCarLine } from "react-icons/ri";
 import { TbCurrencyDollarOff } from "react-icons/tb";
 import { SiFoursquarecityguide } from "react-icons/si";
 import { benefits } from "../assets";
-import { fadeIn } from "../utils/motion";
+import { fadeIn, slideIn } from "../utils/motion";
 import { motion } from "framer-motion";
 
 const Benefits = () => {
@@ -25,52 +25,52 @@ const Benefits = () => {
             <div className="icon mr-2">
               <BsHeadset size="3em" className="icon" />
             </div>
-            <div className="text">
+            <motion.div variants={slideIn("right", "", 0.1, 1)}>
               <h1 className={`${styles.sectionSubText}`}>24/7 Services</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                 porro, explicabo excepturi molestias sequi repellat illum{" "}
               </p>
-            </div>
+            </motion.div>
           </div>
 
           <div className="flex flex-row mb-10">
             <div className="icon mr-2">
               <RiCarLine size="3em" className="icon" />
             </div>
-            <div className="text">
+            <motion.div variants={slideIn("right", "", 0.2, 1)}>
               <h1 className={`${styles.sectionSubText}`}>Airport Car</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                 porro, explicabo excepturi molestias sequi repellat illum{" "}
               </p>
-            </div>
+            </motion.div>
           </div>
 
           <div className="flex flex-row mb-10">
             <div className="icon mr-2">
               <TbCurrencyDollarOff size="3em" className="icon" />
             </div>
-            <div className="text">
+            <motion.div variants={slideIn("right", "", 0.3, 1)}>
               <h1 className={`${styles.sectionSubText}`}>Special Discount</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                 porro, explicabo excepturi molestias sequi repellat illum{" "}
               </p>
-            </div>
+            </motion.div>
           </div>
 
           <div className="flex flex-row mb-10">
             <div className="icon mr-2">
               <SiFoursquarecityguide size="3em" className="icon" />
             </div>
-            <div className="text">
+            <motion.div variants={slideIn("right", "", 0.4, 1)}>
               <h1 className={`${styles.sectionSubText}`}>Your Guide</h1>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
                 porro, explicabo excepturi molestias sequi repellat illum{" "}
               </p>
-            </div>
+            </motion.div>
           </div>
         </div>
         <div className="image">
